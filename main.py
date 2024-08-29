e6790e9ad019fbe32179135a62bd65e9193aa995
Adam <justfansof@gmail.com>
2024-08-29 15:24:43 -0400

    update link

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     os.mkdir('dist')
 
     with open('dist/CNAME', 'w') as f:
-        f.write('tshr.me')
+        f.write('jfo.gg')
 
     for link in links:
         html_document = html.format(url=link['url'])
